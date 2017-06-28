3db3b7be01c38a7373f40640c0cbd0a731ae569a
chantra choeung <chantra.choeung@gmail.com>
2017-06-28 20:09:53 +0700

    my

--- page/footer.php
+++ page/footer.php
@@ -1,59 +1,63 @@
 
         <footer class="footer footer-four">
-            <div class="primary-footer brand-bg text-center">
+            <div class="primary-footer gray-bg text-center">
                 <div class="container">
-
-                  <a href="#top" class="page-scroll btn-floating btn-large pink back-top waves-effect waves-light" data-section="#top">
-                    <i class="material-icons">&#xE316;</i>
-                  </a>
-
-                  <ul class="social-link tt-animate ltr mt-20">
-                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                    <li><a href="#"><i class="fa fa-tumblr"></i></a></li>
-                    <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-                    <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
-                    <li><a href="#"><i class="fa fa-instagram"></i></a></li>
-                    <li><a href="#"><i class="fa fa-rss"></i></a></li>
-                  </ul>
-
                   <hr class="mt-15">
-
                   <div class="row">
-                    <div class="col-md-12">
-                          <div class="footer-logo">
-                            <img src="assets/img/logo-white.png" alt="">
-                          </div>
+                    <div class="col-md-3 col-sm-6">
+                          <h2 class="light-159d80-text font-size-25">អំពីក្រុមហ៊ុន</h2>
+                          <p class="black-text"><span class="light-159d80-text">TECH CAMBO </span> <span class="font-size-18">  ជាក្រុមដែលមានជំនាញផ្តល់សេវាកម្មផ្នែកបច្ចេកវិទ្យា </span> </p>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                        <h2 class="light-159d80-text font-size-25">ផលិតផល</h2>
 
-                          <span class="copy-text">Copyright &copy; 2016 <a href="#">Materialize</a> &nbsp; | &nbsp;  All Rights Reserved &nbsp; | &nbsp;  Designed By <a href="#">TrendyTheme</a></span>
-                          <div class="footer-intro">
-                            <p>Penatibus tristique velit vestibulum adipiscing habitant aenean feugiat at condimentum aptent odio orci vulputate hac mollis a.Vestibulum adipiscing gravida justo a ac euismod vitae.</p>
-                          </div>
-                    </div><!-- /.col-md-12 -->
+                          <ul class="cambo-menu cambo-pad-l-20">
+                            <li><a href="#" class="black-text"><i class="fa fa-check-square-o light-159d80-text"></i> &nbsp;  <span class="font-size-18">ប្រព័ន្ធគ្រប់គ្រងម៉ាតនិងហាងលក់ចាបហួយ</span></a></li>
+                            <li><a href="#" class="black-text"><i class="fa fa-check-square-o light-159d80-text"></i> &nbsp; <span class="font-size-18">ប្រព័ន្ធគ្រប់គ្រងភោជនីយដ្ឋាននិងកាហ្វេ</span></a></li>
+                            <li><a href="#" class="black-text"><i class="fa fa-check-square-o light-159d80-text"></i> &nbsp; <span class="font-size-18">ប្រព័ន្ធគ្រប់គ្រងសាលារៀន</span></a></li>
+                          
+                          
+                        </ul>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                        <h2 class="light-159d80-text font-size-25"> សេវាកម្ម </h2>
+
+                         <ul class="cambo-menu cambo-pad-l-20">
+                            <li><a href="#" class="black-text"><i class="fa fa-check-square-o light-159d80-text"></i> &nbsp; <span class="font-size-18">សេវាបង្កើត ប្រព័ន្ធគ្រប់គ្រង</span></a></li>
+                            <li><a href="#" class="black-text"><i class="fa fa-check-square-o light-159d80-text"></i> &nbsp; <span class="font-size-18">សេវាបង្កើត </span>Website</a></li>
+                            
+                            <li><a href="#" class="black-text"><i class="fa fa-check-square-o light-159d80-text"></i>  &nbsp; <span class="font-size-18">សេវា</span> Design Logo <span class="font-size-18">ក្រុមហ៊ុន</span></a></li>
+                            <li><a href="#" class="black-text"><i class="fa fa-check-square-o light-159d80-text"></i> &nbsp; <span class="font-size-18">សេវាតបណ្តាញ </span> Network</a></li>
+                            <li><a href="#" class="black-text"><i class="fa fa-check-square-o light-159d80-text"></i>  &nbsp; <span class="font-size-18">សេវាដំលើងកាមេរ៉ាសុវត្ដិភាព</span></a></li>
+                          
+                        </ul>
+                        
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                          <h2 class="light-159d80-text font-size-25"> ទំនាក់ទំនង </h2>
+                           <ul class="cambo-menu cambo-pad-l-20">
+                            <li><a href="#" class="black-text"> <i class="fa fa-phone light-159d80-text" aria-hidden="true"></i> &nbsp; <span class="font-size-18">០១០ ៥៩០ ២១៣ </span> / <span class="font-size-18"> ០៨៥ ៥០០ ១២៨ </span></a></li>
+                            <li><a href="#" class="black-text"><i class="fa fa-envelope-o light-159d80-text" ></i>  &nbsp; service.techcambo@gmail.com</a></li>
+                            <li><a href="#" class="black-text"><i class="fa fa-link light-159d80-text" ></i>  &nbsp; www.techcambo.com</a></li>
+                       
+                          
+                        </ul>
+                    </div>
                   </div><!-- /.row -->
                 </div><!-- /.container -->
+                <hr class="mt-15">
             </div><!-- /.primary-footer -->
-
-            <div class="secondary-footer brand-bg darken-2 text-center">
+              
+            <div class="secondary-footer light-159d80 darken-2 text-center">
                 <div class="container">
                     <ul>
-                      <li><a href="#">Home</a></li>
-                      <li><a href="#">About us</a></li>
-                      <li><a href="#">Services</a></li>
-                      <li><a href="#">Portfolio</a></li>
-                      <li><a href="#">Contact us</a></li>
+                      <li><a href="#"><span class="font-size-18">ទំព័រដើម</span></a></li>
+                      <li><a href="#"><span class="font-size-18">ផលិតផល</span></a></li>
+                      <li><a href="#"><span class="font-size-18">សេវាកម្ម</span></a></li>
+                      <li><a href="#"><span class="font-size-18">អំពីពួកយើង</span></a></li>
+                      <li><a href="#"><span class="font-size-18">ទំនាក់ទំនង</span></a></li>
                     </ul>
                 </div><!-- /.container -->
             </div><!-- /.secondary-footer -->
         </footer>
 
-         <!-- Preloader -->
-        <div id="preloader">
-          <div class="preloader-position"> 
-            <img src="assets/img/logo-colored.png" alt="logo" >
-            <div class="progress">
-              <div class="indeterminate"></div>
-            </div>
-          </div>
-        </div>
-        <!-- End Preloader --> 
--- page/header.php
+++ page/header.php
@@ -3,26 +3,25 @@
     <div class="container">
         <div class="row">
             <!-- Social Icon -->
-            <div class="col-md-6">
+            <div class="col-md-4">
                 <!-- Social Icon -->
                 <ul class="list-inline social-top tt-animate btt">
                     <li><a href="#"><i class="fa fa-facebook"></i></a></li>
                     <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                    <li><a href="#"><i class="fa fa-tumblr"></i></a></li>
                     <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-                    <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+                
                     <li><a href="#"><i class="fa fa-instagram"></i></a></li>
-                    <li><a href="#"><i class="fa fa-rss"></i></a></li>
+                  
                 </ul>
             </div>
 
-            <div class="col-md-6 text-right">
+            <div class="col-md-8 text-right">
                 <ul class="topbar-cta no-margin">
                     <li class="mr-20">
-                        <a><i class="material-icons mr-10">&#xE0B9;</i>info@materialize.com</a>
+                        <a><i class="material-icons mr-10">&#xE0B9;</i>service.techcambo@gmail.com</a>
                     </li>
                     <li>
-                        <a><i class="material-icons mr-10">&#xE0CD;</i> +01 123 456 78</a>
+                        <a><i class="material-icons mr-10">&#xE0CD;</i><span class="font-size-18"> ០១០ ៥៩០ ២១៣ </span> / <span class="font-size-18"> ០៨៥ ៥០០ ១២៨ </span></a>
                     </li>
                 </ul>
             </div>
@@ -62,37 +61,38 @@
                 <!--mega menu start-->
                 <ul class="menuzord-menu pull-right">
                     <li class="active">
-                        <a href="javascript:void(0)">Home</a>
+                        <a href="javascript:void(0)"><span class="font-size-18">ទំព័រដើម</span></a>
                     </li>
-                    <li><a href="javascript:void(0)">Software</a>
+                    <li><a href="javascript:void(0)"><span class="font-size-18">ផលិតផល</span></a>
                         <ul class="dropdown">
                             <li>
-                                <a href="#">POS</a>
+                                <a href="#"><span class="font-size-18">ប្រព័ន្ធគ្រប់គ្រងម៉ាតនិងហាងលក់ចាបហួយ</span></a>
                             </li>
                             <li>
-                                <a href="#">Mini Mart/Shop</a>
+                                <a href="#"><span class="font-size-18">ប្រព័ន្ធគ្រប់គ្រងភោជនីយដ្ឋាននិងកាហ្វេ</span></a>
                             </li>
                             <li>
-                                <a href="#">School Management</a>
+                                <a href="#"><span class="font-size-18">ប្រព័ន្ធគ្រប់គ្រងសាលារៀន</span></a>
                             </li>
                         </ul>
                     </li>
-                    <li><a href="javascript:void(0)">Services</a>
+                    <li><a href="javascript:void(0)"><span class="font-size-18">សេវាកម្ម</span></a>
                         <ul class="dropdown">
-                            <li>
-                                <a href="#">Network</a>
-                            </li>
-                            <li>
-                                <a href="#">Camera Security</a>
-                            </li>
+                           <li><a href="#" > <span class="font-size-18">សេវាបង្កើត ប្រព័ន្ធគ្រប់គ្រង</span></a></li>
+                            <li><a href="#" ><span class="font-size-18">សេវាបង្កើត </span>Website</a></li>
+                            
+                            <li><a href="#" ><span class="font-size-18">សេវា</span> Design Logo <span class="font-size-18">ក្រុមហ៊ុន</span></a></li>
+                            <li><a href="#" ><span class="font-size-18">សេវាតបណ្តាញ </span> Network</a></li>
+                            <li><a href="#" ><span class="font-size-18">សេវាដំលើងកាមេរ៉ាសុវត្ដិភាព</span></a></li>
+                          
                         </ul>
                     </li>
                     <li>
-                        <a href="javascript:void(0)">About us</a>
+                        <a href="javascript:void(0)"><span class="font-size-18">អំពីពួកយើង</span></a>
                     </li>
 
                     <li>
-                        <a href="javascript:void(0)">Contact us</a>
+                        <a href="javascript:void(0)"><span class="font-size-18">ទំនាក់ទំនង</span></a>
                     </li>
                 </ul>
                 <!--mega menu end-->
